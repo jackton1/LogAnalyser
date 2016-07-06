bf736d856cb9feb94924be8b58df9a75519b8549
jackton1 <jtonye@mircomgroup.com>
2016-07-05 23:32:51 -0400

    Modified the counter to read directly from .log file

--- HB Analyzer/Python Log Analyzer/counter.py
+++ HB Analyzer/Python Log Analyzer/counter.py
@@ -13,17 +13,32 @@ from subprocess import call
 ##
 ##c_program()
 
-
 def source_file():
     #call(["./LogAnalyzer", "args", "to", "LogAnalyzer.exe"])
     file = input("Enter File name >")
-    with open(file +'.txt') as file:
-        lines = file.read().splitlines()
-    file.close()
-    create_file(lines)
+    mac_address = []
+    time = []
+    if file[:7] == 'Message':
+        date = file[8:]
+        with open(file +'.log') as file:
+            for word in file:
+                if word[81:83] == 'RX':   ##Check if the messages are RX
+                    time.append(word[11:16])  ##Append the time the messages were sent by the device
+                    mac_address.append(word[116:139])  ##Analyze the log file and add each Source MAC address
+            file.close()
+        total_devices = list(set(mac_address)) ##get each of the MAC Address that were recorded in the log
+        for i,j in enumerate(mac_address):  ##get the index and each MAC address with RX in the Log
+            for item in total_devices:  ##Compare the item with the list of available address
+                if j == item:
+                    #print("MAC:{} Time:{}".format(item,time[i])
+                    break;
+        print(date)
+        ##lines = time
+        #print(total_devices)
 
-def create_file(lines):
-    date = input("Enter the Test Date of Log like DD Month YYYY>")
+def create_file(lines, date):
+    print("Test Date your Analysing is {}".format(date))
+    print("The location of the log file is ../../Logs/"+ date +" Micare Report.doc")
     data = list(lines)
     m = Counter(data)
     string = repr(m)
@@ -44,8 +59,8 @@ def create_file(lines):
         string2 = list(m.items())
         string2.sort()
         for item in string2:
-            stnd= str(item).strip('{[)]}')
-            f.write(stnd + " Messages where sent)\n")
+            sent= str(item).strip('{[)]}')
+            f.write(sent + " Messages where sent)\n")
         f.write("\n\n\n"+"*" * 15)
         f.write("Python 3.5.1 Auto Generated Log Data Report")
         f.write("*" * 15)
@@ -57,7 +72,8 @@ def create_file(lines):
     except PermissionError:
         print("File is open or can't be accessed !!!")
         start()
-        
+
+       
 def start():   
     print("***Welcome to Python Log Generator***\n")
     try:
